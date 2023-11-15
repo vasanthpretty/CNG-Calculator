c13d6b20ff619f347a7b3c902fc68cc7547aa6bf
vasanthpretty <vasanth.pretty@gmail.com>
2023-11-15 12:13:44 +0530

    js added

---
+++ assets/css/learning.js
@@ -0,0 +1,26 @@
+
+// Creating an object
+// const vasanthCar={
+//     color:"MattGrey",
+//     mFD:2021,
+//     model:"Benz",
+//     isRegitered:true,
+//     seatingCapacity:5,
+// //methods inside an objects
+// carCondition(){
+//     console.log("Your car is in Awesome condition");
+// },
+
+// vehicleAge(){
+// //    const age= new Date().getFullYear()-this.mFD;
+// //      return age;
+//     return new Date().getFullYear()-vasanthCar.mFD;
+// }};
+// // delete vasanthCar.seatingCapacity; // deleting an key and value from an object
+// // console.log(vasanthCar);
+// // vasanthCar ["isimported"]=true;  // inserting an key and value to an object 
+// // console.log(vasanthCar);
+
+// console.log(vasanthCar.vehicleAge());
+// vasanthCar.carCondition();
+
