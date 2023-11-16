b5e3373c12c8973366f3e6251b66510b18106b6f
vasanthpretty <vasanth.pretty@gmail.com>
2023-11-16 10:46:08 +0530

    js if else added

--- assets/css/main.js
+++ assets/css/main.js
@@ -6,18 +6,33 @@ const distanceEl=document.getElementById("Trip_distance");
 const efficienyEl=document.querySelector("#Fuel_Efficiency");
 const costEl=document.querySelector("#Fuel_cost");
 const btnEl=document.querySelector("#CalculateBtn");
+const Form=document.querySelector("#my-form");
 
 
 
 btnEl.addEventListener("click",(e)=>{
-    e.preventDefault();
+    if(distanceEl.value===""){
+        alert("enter the trip distance to calculate the value");
+    }
+
+    else if(costEl.value===""){
+alert("enter the Fuel Cost to calculate the value");
+
+    }
+
+    else {
+        e.preventDefault();
     const DistanceKm=distanceEl.value;
     const FuelEff=efficienyEl.value;
     const FuelCost=costEl.value;
     
     const result= (DistanceKm / FuelEff)*FuelCost;
-    
-    console.log(result);
+      
+    alert("your estimated trip amount  "+Math.round(result) );
+   
+    //resetiing the form value
+    Form.reset();
+    } 
     
 })
 
